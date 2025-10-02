425a6196006b2a0a6e97ff1a9a490b9d40e6a2d6
Josh Long <54473+joshlong@users.noreply.github.com>
2025-10-02 12:06:19 -0600

    up

--- assistant/src/main/java/com/example/assistant/AssistantApplication.java
+++ assistant/src/main/java/com/example/assistant/AssistantApplication.java
@@ -6,12 +6,12 @@ import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +19,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -32,39 +33,6 @@ public class AssistantApplication {
         SpringApplication.run(AssistantApplication.class, args);
     }
 
-
-    @Bean
-    QuestionAnswerAdvisor questionAnswerAdvisor(VectorStore vectorStore) {
-        return new QuestionAnswerAdvisor(vectorStore);
-    }
-
-    @Bean
-    PromptChatMemoryAdvisor memory(DataSource dataSource) {
-        var jdbc = JdbcChatMemoryRepository
-                .builder()
-                .dataSource(dataSource)
-                .build();
-        var mwa = MessageWindowChatMemory
-                .builder()
-                .chatMemoryRepository(jdbc)
-                .build();
-        return PromptChatMemoryAdvisor
-                .builder(mwa)
-                .build();
-    }
-
-    //    @Bean
-    ApplicationRunner vectorize(VectorStore vectorStore, DogRepository repository) {
-        return _ -> repository
-                .findAll()
-                .forEach(dog -> {
-                    var dogument = new Document("id: %s, name: %s, description: %s".formatted(
-                            dog.id(), dog.name(), dog.description()
-                    ));
-                    vectorStore.add(List.of(dogument));
-                });
-    }
-
     @Bean
     McpSyncClient scheduler() {
         var mcp = McpClient
@@ -74,15 +42,27 @@ public class AssistantApplication {
         return mcp;
     }
 
+    @Bean
+    QuestionAnswerAdvisor questionAnswerAdvisor(VectorStore vectorStore) {
+        return new QuestionAnswerAdvisor(vectorStore);
+    }
+
+    @Bean
+    PromptChatMemoryAdvisor promptChatMemoryAdvisor(DataSource d) {
+
+        var jdbc = JdbcChatMemoryRepository
+                .builder()
+                .dataSource(d)
+                .build();
+        var mwa = MessageWindowChatMemory
+                .builder()
+                .chatMemoryRepository(jdbc)
+                .build();
+        return PromptChatMemoryAdvisor
+                .builder(mwa)
+                .build();
+    }
 }
-
-
-interface DogRepository extends ListCrudRepository<Dog, Integer> {
-}
-
-record Dog(@Id int id, String name, String owner, String description) {
-}
-
 
 @Controller
 @ResponseBody
@@ -91,16 +71,28 @@ class AssistantController {
     private final ChatClient ai;
 
     AssistantController(
-            List<McpSyncClient> scheduler,
-            PromptChatMemoryAdvisor memoryAdvisor,
+            McpSyncClient scheduler,
+            DogRepository repository, VectorStore vs,
+            ChatClient.Builder ai,
             QuestionAnswerAdvisor questionAnswerAdvisor,
-            ChatClient.Builder chatClient) {
-        this.ai = chatClient
-                .defaultAdvisors(memoryAdvisor, questionAnswerAdvisor)
+            PromptChatMemoryAdvisor advisor) {
+
+        if (false)
+            repository.findAll().forEach(dog -> {
+                var dogument = new Document(
+                        "id:%s, name: %s, description: %s".formatted(
+                                dog.id(), dog.name(), dog.description()
+                        ));
+                vs.add(List.of(dogument));
+
+            });
+
+        this.ai = ai
+                .defaultAdvisors(advisor, questionAnswerAdvisor)
                 .defaultToolCallbacks(new SyncMcpToolCallbackProvider(scheduler))
                 .defaultSystem("""
                         You are an AI powered assistant to help people adopt a dog from the adoption\s
-                        agency named Pooch Palace with locations in Oslo, Crested Butte, Seoul, Tokyo, Singapore, Paris,\s
+                        agency named Pooch Palace with locations in Colorado Springs, Seoul, Tokyo, Singapore, Paris,\s
                         Mumbai, New Delhi, Barcelona, San Francisco, and London. Information about the dogs available\s
                         will be presented below. If there is no information, then return a polite response suggesting we\s
                         don't have any dogs available.
@@ -108,15 +100,28 @@ class AssistantController {
                 .build();
     }
 
-    @GetMapping("/ask")
-    String ask(@RequestParam String question) {
+    @GetMapping("/{user}/assistant")
+    String ask(@PathVariable String user,
+               @RequestParam String question) {
         return this.ai
-                .prompt(question)
+                .prompt()
+                .user(question)
+                .advisors(a -> a
+                        .param(ChatMemory.CONVERSATION_ID, user))
                 .call()
                 .content();
+        //.entity(DogAdoptionSuggestion.class);
+
     }
+
 }
 
 
-record DogAdoptionSuggestion(int id, String name, String description) {
+record DogAdoptionSuggestion(String description, int id, String name) {
+}
+
+interface DogRepository extends ListCrudRepository<Dog, Integer> {
+}
+
+record Dog(@Id int id, String name, String owner, String description) {
 }
